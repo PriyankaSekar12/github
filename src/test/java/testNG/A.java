2b48363f712e84d2d42f6624f1b59edb43a6e488
PriyankaSekar12 <priyasekar1212@gmail.com>
2023-04-14 20:58:08 +0530

    push codes

---
+++ src/test/java/testNG/A.java
@@ -0,0 +1,39 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class A {
+
+			
+			@Test
+			private void test1() {
+				System.out.println("test1");
+			}
+			@Test
+			private void test2() {
+				System.out.println("test2");
+			}
+			@Test
+			private void test3() {
+				System.out.println("test3");
+			}
+			@Test
+			private void test4() {
+				System.out.println("test4");
+			}
+			@Test
+			private void test5() {
+				System.out.println("test5");
+			}
+			@Test
+			private void test6() {
+				System.out.println("test6");
+			}
+
+		}
+
+
+
+
+
+
